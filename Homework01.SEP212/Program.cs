6815aef4b0f952a209bb7288b3e20f244ba82fd0
Admin <Admin@DESKTOP-2DFAO40>
2022-08-16 23:34:55 +0600

    Добавьте файлы проекта.

---
+++ Homework01.SEP212/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework01.SEP212
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Задание № 1
+
+            //Console.Write("Введите ФИО: ");
+            //string userName = Console.ReadLine();
+            //Console.WriteLine("Приветствую тебя: " +  userName);
+
+
+
+            //  Задание № 2
+
+            //Console.Write("Введите первое число: ");
+            //string Number1 = Console.ReadLine();
+
+
+            //int Numb = Convert.ToInt32(Number1);
+
+            //Console.Write("Введите второе число: ");
+            //string Number2 = Console.ReadLine();
+
+            //int Numb1 = Convert.ToInt32(Number2);
+
+            //int sum = Numb + Numb1;
+
+            //Console.WriteLine("Сумма двух чисел = " + sum);
+
+        }
+    }
+}
